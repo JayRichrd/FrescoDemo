523fd8fc2aeb5f42f1f69ae216c5cf3536d6cdb7
cainjiang <cainjiang@tencent.com>
2018-09-03 08:13:48 +0800

    Initial commit

---
+++ app/src/main/java/fresco/cain/tencent/com/frescodemo/MainActivity.kt
@@ -0,0 +1,21 @@
+package fresco.cain.tencent.com.frescodemo
+
+import android.net.Uri
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.facebook.drawee.view.SimpleDraweeView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        loadImg()
+    }
+
+    private fun loadImg() {
+        val uri = Uri.parse("https://www.gstatic.com/webp/gallery/1.sm.jpg")
+        val simpleDraweeView = findViewById<SimpleDraweeView>(R.id.sdv_image_view)
+        simpleDraweeView.setImageURI(uri)
+    }
+}
